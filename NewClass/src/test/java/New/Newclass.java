c10eea076c8a54dfeb36b852537b09a0bcd197b8
AbhayKumarAlok <109574810+AbhayKumarAlok@users.noreply.github.com>
2022-07-19 17:55:06 +0530

    Update Newclass.java

--- NewClass/src/test/java/New/Newclass.java
+++ NewClass/src/test/java/New/Newclass.java
@@ -11,7 +11,10 @@ public class Newclass {
 		System.out.println("New vgffNMbajJ");
 		System.out.println("New vgffNMbajJ");System.out.println("New vgffNMbajJ");
 		System.out.println("New vgffNMbajJ");
-		
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
 		
 
 	}
