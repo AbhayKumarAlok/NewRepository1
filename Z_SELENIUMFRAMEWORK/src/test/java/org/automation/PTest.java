6839a2c2b7fb334453faf689182e766f4c167b45
admin dell <admin dell@DESKTOP-80HLJ8Q>
2022-07-20 16:54:58 +0530

    hiiiiiii

---
+++ Z_SELENIUMFRAMEWORK/src/test/java/org/automation/PTest.java
@@ -0,0 +1,24 @@
+package org.automation;
+
+import org.testng.annotations.Test;
+
+public class PTest 
+{
+	@Test
+	public void test()
+	{
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+	}
+
+
+}
